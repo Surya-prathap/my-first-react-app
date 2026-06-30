8fecffee9ed0be394ccdb8cb325cccca297d44ad
Surya-prathap <13794prathap@gmail.com>
2026-06-30 15:00:00 +0530

    components in react

---
+++ src/assets/components/Body.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Body() {
+  return (
+    <>
+     <h2>Name: Surya Prathap Reddy</h2>
+     <h3>Java Full Stack Developer</h3>
+     <h3>Batch-3</h3>
+     <h3>React Classes By Ratan Sir...</h3> 
+    </>
+  )
+}
+
+export default Body
